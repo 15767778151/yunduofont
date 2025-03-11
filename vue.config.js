62c984152333fd156ff7fbd653c3194f7c80664e
zhoushan <lizhenshan@linghit.com>
2025-03-11 13:07:55 +0800

    配置 GitHub Pages 部署

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,12 @@ module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/yunduofont/'  // 替换为你的仓库名
     : '/',
-  outputDir: 'dist'
+  outputDir: 'dist',
+  plugins: [vue()],  
+  base: './',  
+  resolve: {  
+    alias: {  
+      '@': fileURLToPath(new URL('./src', import.meta.url))  
+    }  
+  }  
 })
