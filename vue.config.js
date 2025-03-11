05a344e4fd445a54c4a9c654f908f3d73073fda3
zhoushan <lizhenshan@linghit.com>
2025-03-11 13:12:33 +0800

    配置 GitHub Pages 部署

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/yunduofont/'  // 替换为你的仓库名
+    ? '/yunduofont/'
     : '/',
-  outputDir: 'dist',
-  plugins: [vue()],  
-  base: './',  
-  resolve: {  
-    alias: {  
-      '@': fileURLToPath(new URL('./src', import.meta.url))  
-    }  
-  }  
+  outputDir: 'dist'
 })
